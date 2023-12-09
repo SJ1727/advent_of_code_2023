b243147acb98f9e68146229fa90f3de55d500fc6
SJ1727 <samueljohnson1727@gmail.com>
2023-12-09 05:18:10 +0000

    Day 9 (>_<)

---
+++ Day 9/task1.py
@@ -0,0 +1,31 @@
+import re
+
+def next_seq_l(seq):
+    n_seq = []
+    for i in range(len(seq) - 1):
+        n_seq.append(seq[i+1] - seq[i])
+        
+    return n_seq
+
+total = 0
+
+with open("day 9\\data.txt") as file:
+    for line in file.readlines():
+        next_seqs = []
+        
+        seq = list(map(lambda x: int(x), line.split()))
+        
+        next_seqs.append(seq)
+        next_seq = seq
+        
+        while any(next_seq := next_seq_l(next_seq)):
+            next_seqs.append(next_seq)
+        next_seqs.append(next_seq)
+        
+        r = 0
+        for s in next_seqs[::-1]:
+            r += s[-1]
+        
+        total += r
+        
+print(total)
