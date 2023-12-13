0dc5154e2cdf8c52f61c75069802fbfb45123469
SJ1727 <samueljohnson1727@gmail.com>
2023-12-13 07:07:47 +0000

    Day 13 ^ω^

---
+++ Day 13/task1.py
@@ -0,0 +1,31 @@
+def mirrow_pos(map_):
+    val = True
+    for i in range(len(map_) - 1):
+        for j in range(len(map_)):
+            val = True
+            if (2 * i) - j + 1 in range(len(map_)):
+                if map_[j] != map_[2 * i - j + 1]:
+                    val = False
+                    break
+        if val:
+            return i
+        
+    return None
+
+maps = []
+s = []
+
+total = 0
+with open("Day 13\\data.txt", "r") as file:
+    text = file.read().split("\n\n")
+    maps = [["".join(list(t)) for t in x.split("\n")] for x in text]
+    maps = [["".join(s) for s in map_] for map_ in maps]
+    
+    for map_ in maps:
+        if (row := mirrow_pos(map_)) is not None:
+            total += (row + 1) * 100
+            continue
+        if (col := mirrow_pos(list(map(lambda x: list(x), zip(*map_))))) is not None:
+            total += (col + 1)
+
+print(total)
